3be3d56b61ffa87f7b77635ba6906c0f4bca4283
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-07-12 23:05:22 -0500

    phase-two module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PhaseOneModule } from './phase-one/phase-one.module';
+import { PhaseTwoModule } from './phase-two/phase-two.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PhaseOneModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PhaseOneModule,
+    PhaseTwoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
