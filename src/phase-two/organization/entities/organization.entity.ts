2cadc1111053854a2ab4f29ffc1fcc36481c736f
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-07-12 23:35:31 -0500

    organization entity created

--- src/phase-two/organization/entities/organization.entity.ts
+++ src/phase-two/organization/entities/organization.entity.ts
@@ -1,1 +1,13 @@
-export class Organization {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('organization')
+export class Organization {
+  @PrimaryColumn({ name: 'id_organization' })
+  id_organization: number;
+
+  @Column({ type: 'varchar', default: null })
+  name: string;
+
+  @Column({ type: 'varchar', default: null })
+  status: number;
+}
