c31128deec9b65c36ba15409c08da6b9bd77dc0b
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-07-12 23:50:53 -0500

    entity repository created and join with tribe

--- src/phase-two/repositories/entities/repository.entity.ts
+++ src/phase-two/repositories/entities/repository.entity.ts
@@ -1,1 +1,31 @@
-export class Repository {}
+import { Tribe } from '../../tribe/entities/tribe.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+
+@Entity('repository')
+export class Repository {
+  @PrimaryColumn({ name: 'id_repository' })
+  id_repository: number;
+
+  @Column({ type: 'varchar', default: null })
+  name: string;
+
+  @Column({ type: 'varchar', length: 1, default: null })
+  status: string;
+
+  @Column({ type: 'varchar', length: 1, default: null })
+  state: string;
+
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  created_at: Date;
+
+  @ManyToOne(() => Tribe, (tribe) => tribe.id_tribe)
+  @JoinColumn({ name: 'id_tribe' })
+  id_tribe: Tribe;
+}
--- src/phase-two/tribe/entities/tribe.entity.ts
+++ src/phase-two/tribe/entities/tribe.entity.ts
@@ -1,9 +1,17 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryColumn,
+} from 'typeorm';
 import { Organization } from '../../organization/entities/organization.entity';
+import { Repository } from '../../repositories/entities/repository.entity';
 
 @Entity('tribe')
 export class Tribe {
-  @PrimaryColumn({ name: 'id_organization' })
+  @PrimaryColumn({ name: 'id_tribe' })
   id_tribe: number;
 
   @Column({ type: 'varchar', default: null })
@@ -15,4 +23,7 @@ export class Tribe {
   @ManyToOne(() => Organization, (organization) => organization.id_organization)
   @JoinColumn({ name: 'id_organization' })
   id_organization: Organization;
+
+  @OneToMany(() => Repository, (repository) => repository.id_repository)
+  a_stations_id: Repository[];
 }
